ac5210cf87652e5a966d807dfba0e7b82fd0af76
João Alberto <joao.alb@usp.br>
2022-08-06 22:31:39 -0300

    Spaces to tabs correction (yes that's what you heard)

--- hide/beam/gaussian_fwhm.py
+++ hide/beam/gaussian_fwhm.py
@@ -24,75 +24,75 @@ import healpy as hp
 from hope import jit
 
 def load_beam_profile(beam_spec, frequencies, params):
-    """
-    Creates a 2d gaussian beam profile for the given params definition 
-    
-    :param params: The params instance with the paramterization
-    
-    :returns profile: A list of callable beam profiles
-    """
+	"""
+	Creates a 2d gaussian beam profile for the given params definition 
+	
+	:param params: The params instance with the paramterization
+	
+	:returns profile: A list of callable beam profiles
+	"""
 
-    beam_profiles = []
-    beam_norms = []
-    for k, frequency in enumerate(frequencies):
-        fwhm = params.fwhm_0 * params.beam_frequency_min / frequency
-        sigma = fwhm / (2. * np.sqrt(2. * np.log(2)))
-        beam_norms.append(normalization(sigma, params.beam_nside))
-        beam_profiles.append(gauss_wrapper(sigma, params.beam_response, params))
-    return beam_profiles, beam_norms
+	beam_profiles = []
+	beam_norms = []
+	for k, frequency in enumerate(frequencies):
+		fwhm = params.fwhm_0 * params.beam_frequency_min / frequency
+		sigma = fwhm / (2. * np.sqrt(2. * np.log(2)))
+		beam_norms.append(normalization(sigma, params.beam_nside))
+		beam_profiles.append(gauss_wrapper(sigma, params.beam_response, params))
+	return beam_profiles, beam_norms
 
 # def _2d_gauss(sigma, beam_response):
-#     i2sigma2 = 1. / (2*sigma**2)
-#     def wrapped(i,j):
-# #         Z =  np.exp(-i**2/(2*sigma**2) - j**2/(2*sigma**2))
-#         Z =  np.exp(i2sigma2 * (-i**2 - j**2))
-#         return Z / Z.sum() * beam_response
-#     return wrapped
+#	 i2sigma2 = 1. / (2*sigma**2)
+#	 def wrapped(i,j):
+# #		 Z =  np.exp(-i**2/(2*sigma**2) - j**2/(2*sigma**2))
+#		 Z =  np.exp(i2sigma2 * (-i**2 - j**2))
+#		 return Z / Z.sum() * beam_response
+#	 return wrapped
 
 
 # cumbersome call to avoid mem leak in hope
 def gauss_wrapper(sigma, beam_response, params):
-    i2sigma2 = 1. / (2*sigma**2)
-    def wrapped(i,j):
-        Z = np.empty_like(i)
-        hope_gauss(i2sigma2, beam_response, i, j, Z)
-        
-        plot_beam = True
-        fits_writting = True
-        if plot_beam:
-		    base_name = "beam_nside{nside}_gauss".format(nside=params.beam_nside)
-            if fits_writing:
-                    import astropy.io.fits as pyfits
-                    fitsfile = params.output_path + base_name + "_healpix.fits"
-		    
-		    reso = hp.nside2resol(params.beam_nside, arcmin=True)/10
-		    xysize = 30*5*int(params.beam_nside/128)
-            beam = Z
-            plt.figure(5)
-            theta, phi = np.pi/2+i, j
-            idxs = hp.ang2pix(params.beam_nside, theta,phi)
-            mapa = np.zeros(12*params.beam_nside**2)
-            mapa[idxs]=beam
-            
-            if fits_writing:
-                    pyfits.writeto(fitsfile, mapa,overwrite=True)
-            hp.gnomview(20*np.log10(abs(mapa)), fig=5, cbar=True, unit="Amplitude (dB)",
-                                    title="HIDE Beam - Gaussian FWHM={}".format(sigma),
-                                    rot=(0,0), xsize=xysize, ysize=xysize,
-                                    reso=reso, min=vmin, max=vmax)
-            hp.graticule(local=True) # default interval = 1 deg
-            plt.savefig(params.output_path + base_name.format(interp="linear") + ".png")
-            exit()
-        
-        return Z
-    return wrapped
+	i2sigma2 = 1. / (2*sigma**2)
+	def wrapped(i,j):
+		Z = np.empty_like(i)
+		hope_gauss(i2sigma2, beam_response, i, j, Z)
+		
+		plot_beam = True
+		fits_writting = True
+		if plot_beam:
+			base_name = "beam_nside{nside}_gauss".format(nside=params.beam_nside)
+			if fits_writing:
+					import astropy.io.fits as pyfits
+					fitsfile = params.output_path + base_name + "_healpix.fits"
+			
+			reso = hp.nside2resol(params.beam_nside, arcmin=True)/10
+			xysize = 30*5*int(params.beam_nside/128)
+			beam = Z
+			plt.figure(5)
+			theta, phi = np.pi/2+i, j
+			idxs = hp.ang2pix(params.beam_nside, theta,phi)
+			mapa = np.zeros(12*params.beam_nside**2)
+			mapa[idxs]=beam
+			
+			if fits_writing:
+					pyfits.writeto(fitsfile, mapa,overwrite=True)
+			hp.gnomview(20*np.log10(abs(mapa)), fig=5, cbar=True, unit="Amplitude (dB)",
+									title="HIDE Beam - Gaussian FWHM={}".format(sigma),
+									rot=(0,0), xsize=xysize, ysize=xysize,
+									reso=reso, min=vmin, max=vmax)
+			hp.graticule(local=True) # default interval = 1 deg
+			plt.savefig(params.output_path + base_name.format(interp="linear") + ".png")
+			exit()
+		
+		return Z
+	return wrapped
 
 def normalization(sigma, nside):
-    n = (4 * np.pi) * sigma * sigma
-    pixarea = hp.nside2pixarea(nside, degrees=False)
-    return pixarea/n
+	n = (4 * np.pi) * sigma * sigma
+	pixarea = hp.nside2pixarea(nside, degrees=False)
+	return pixarea/n
 
 @jit
 def hope_gauss(i2sigma2, beam_response, i, j, Z):
-    Z[:] =  np.exp(i2sigma2 * (-i**2 - j**2))
-    #Z /= np.sum(Z) * beam_response
+	Z[:] =  np.exp(i2sigma2 * (-i**2 - j**2))
+	#Z /= np.sum(Z) * beam_response
